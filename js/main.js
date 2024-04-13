4702485c13f88eaa7ba01589bdf02acead09f094
noz <160399039+noz-23@users.noreply.github.com>
2024-04-13 20:34:42 +0900

    main

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-function LinkClick(e) {
-    var link = jQuery(this);
-    console.log('link:%o', link);
-    var iframe = document.getElementById('id_iframe_main');
-    console.log('iframe:%o', iframe);
-    iframe.src = link.src;
-}
-var listLink = document.getElementsByTagName('a');
-console.log('listLink:%o', listLink);
-for (var link of listLink) {
-    link.addEventListener('click', LinkClick);
-    console.log('link:%o', link);
-}
+jQuery.noConflict();
+
+
+(async () => {
+    'use strict';
+    function LinkClick(e) {
+        var link = jQuery(this);
+        console.log('link:%o', link);
+        var iframe = document.getElementById('id_iframe_main');
+        console.log('iframe:%o', iframe);
+        iframe.src = link.src;
+    }
+    var listLink = document.getElementsByTagName('a');
+    console.log('listLink:%o', listLink);
+    for (var link of listLink) {
+        link.addEventListener('click', LinkClick);
+        console.log('link:%o', link);
+    }
+})();

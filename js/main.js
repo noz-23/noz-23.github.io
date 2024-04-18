9262e686588e5df441b28ec0d9751bab8e5a4d31
noz <160399039+noz-23@users.noreply.github.com>
2024-04-18 23:20:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ jQuery.noConflict();
     var listLink = document.getElementsByTagName('a');
     console.log('listLink:%o', listLink);
     var arryLink =Array.from(listLink)
+    console.log('arryLink:%o', arryLink);
     for (var l of arryLink) {
         console.log('link:%o', l);
         l.addEventListener('click', LinkClick);
